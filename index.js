b3f78ef5236bcf81b4b1d734e0187a1249447c5e
Daqsoo <Mr.Spy710@gmail.com>
2023-02-08 16:36:18 +0200

    import jwt,mongoose,connect db, created user model for registration

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongooose from 'mongoose';
+
+mongooose
+.connect('mongodb+srv://admin:6w@cluster0.fispict.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('DB OK'))
+.catch((err)=> console.log('DB Error', err));
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello world');
-});
+app.use(express.json());
+
+app.post('/auth/register', (req,res) =>
+{
+} )
 
 app.listen(4444, (err) => {
     if(err)
