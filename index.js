6bd0f488dd06af97ce98157d99598deda01af22f
Daqsoo <Mr.Spy710@gmail.com>
2023-02-10 15:53:37 +0200

    refactoring code

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ import multer from 'multer';
 
 import {registerValidation,loginValidation,postCreateValidation} from './validations.js'
 
-import checkAuth from "./utils/checkAuth.js";
+import {checkAuth, HandleErrors} from "./utils/index.js";
 
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
+import {UserController,PostController} from './controllers/index.js'
 
 mongooose
 .connect('mongodb+srv://admin:6w@cluster0.fispict.mongodb.net/blog?retryWrites=true&w=majority')
@@ -30,8 +29,8 @@ const upload = multer({storage});
 app.use(express.json());
 app.use('/uploads',express.static('uploads'));
 
-app.post('/auth/login',loginValidation, UserController.login)
-app.post('/auth/register', registerValidation,UserController.register);
+app.post('/auth/login',loginValidation, HandleErrors,UserController.login)
+app.post('/auth/register', registerValidation,HandleErrors,UserController.register);
 app.get('/auth/me',checkAuth, UserController.getMe);
 
 app.post('/upload',checkAuth, upload.single('image'),(req,res) => {
@@ -44,7 +43,7 @@ app.get('/posts',PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts',checkAuth, postCreateValidation,  PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id',checkAuth, PostController.update);
+app.patch('/posts/:id',checkAuth,postCreateValidation, HandleErrors, PostController.update);
 
 app.listen(4444, (err) => {
     if(err)
